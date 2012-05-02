ade1251cc7d464055d7ad85e03ffde591b097328
Caiopia <caio@paperlesspost.com>
2012-05-02 14:45:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c368a2092ecddd6c01d9978882bbb3fae802a6bc6881a65e09c8b220689e760a0ff701905dc7a0addf088426a5b2745eebe92e37cbe50700bf56708ae0d1a347'
